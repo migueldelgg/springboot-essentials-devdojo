0121ec8b6f8d500d508e3a6ef5655339a4e6f8b2
4Miguel2 <delgdevoleper@gmail.com>
2024-03-24 18:11:02 -0300

    spring datajpa configuration

--- src/main/java/com/migueldelgado/springboot2/domain/Anime.java
+++ src/main/java/com/migueldelgado/springboot2/domain/Anime.java
@@ -1,10 +1,19 @@
 package com.migueldelgado.springboot2.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
