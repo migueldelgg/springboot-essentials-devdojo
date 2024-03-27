b49b8e125561893ce6cc2903233fff4a0302c2d2
4Miguel2 <delgdevoleper@gmail.com>
2024-03-27 11:59:26 -0300

    @Transactional

--- src/main/java/com/migueldelgado/springboot2/service/AnimeService.java
+++ src/main/java/com/migueldelgado/springboot2/service/AnimeService.java
@@ -9,6 +9,7 @@ import com.migueldelgado.springboot2.requests.AnimePutRequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -29,6 +30,7 @@ public class AnimeService {
                 .orElseThrow(() -> new BadRequestException("Anime not Found"));
     }
 
+    @Transactional
     public Anime save(AnimePostRequestBody animePostRequestBody) {
         return animeRepository.save(AnimeMapper.INSTANCE.toAnime(animePostRequestBody));
     }
