6ee943c35108f8b0ce5c7289d289b1f2282174b7
4Miguel2 <delgdevoleper@gmail.com>
2024-03-27 11:49:18 -0300

    Handler Exception

---
+++ src/main/java/com/migueldelgado/springboot2/handler/RestExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.migueldelgado.springboot2.handler;
+
+import com.migueldelgado.springboot2.exception.BadRequestException;
+import com.migueldelgado.springboot2.exception.BadRequestExceptionDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice //spring bean ->
+public class RestExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException bre) {
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Check the Documentation")
+                        .details(bre.getMessage())
+                        .developerMsg(bre.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
+}
