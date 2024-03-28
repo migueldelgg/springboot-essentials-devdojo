d769de121995660c5f8f6c759615c65887bb9819
4Miguel2 <delgdevoleper@gmail.com>
2024-03-28 14:43:24 -0300

    Paginacao

--- src/main/java/com/migueldelgado/springboot2/exception/ExceptionDetails.java
+++ src/main/java/com/migueldelgado/springboot2/exception/ExceptionDetails.java
@@ -1,12 +1,16 @@
 package com.migueldelgado.springboot2.exception;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
 @Data
 @SuperBuilder
+@Getter
+@Setter
 public class ExceptionDetails {
 
     protected String title;
